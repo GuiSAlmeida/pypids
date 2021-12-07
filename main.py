2ba54227451a4d9d9432e4d0ef947cf5414fade3
guisalmeida <guilherme.dasilva@linx3.com>
2021-12-07 17:34:25 -0300

    refactor: add utils

--- main.py
+++ main.py
@@ -3,43 +3,17 @@ https://platform.chaordicsystems.com/raas/v2/clients/usaflex/products/115977936
 """
 
 import csv
-import requests
-import base64
 import os
-import json
 from more_itertools import unique_everseen
 from flask import Flask, render_template, request
+from utils import get_plat_product, make_auth
 from dotenv import load_dotenv
 
 load_dotenv()
-
 PLAT_USER = os.getenv('PLAT_USER')
 PLAT_PASSWORD = os.getenv('PLAT_PASSWORD')
 
-auth_str = f'{PLAT_USER}:{PLAT_PASSWORD}'
-auth_bytes = auth_str.encode('ascii')
-base64_bytes = base64.b64encode(auth_bytes)
-PLATFORM_AUTHORIZATION = base64_bytes.decode('ascii')
-
 app = Flask(__name__)
-
-
-def is_plat_product(apikey, id):
-    try:
-        ENDPOINT = f'https://platform.chaordicsystems.com/raas/v2' \
-            f'/clients/{apikey}/products/{id}'
-        response = requests.get(
-            ENDPOINT,
-            headers={'Authorization': f'Basic {PLATFORM_AUTHORIZATION}'}
-        )
-        response_dict = json.loads(response.text)
-
-        if 'AVAILABLE' in response_dict['status']:
-            return True
-
-    except Exception as e:
-        print(f'Error on get_classes: {e}')
-        return False
 
 
 @app.route('/')
@@ -47,10 +21,13 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/add', methods=['POST'])
+@app.route('/', methods=['POST'])
 def add():
     file = request.files.get("file")
     apikey = request.form.get('apikey')
+    parsed_list = []
+    sanitized = 0
+    plat_authorization = make_auth(PLAT_USER, PLAT_PASSWORD)
 
     filename = file.filename
     file.save(os.path.join('data', filename))
@@ -60,13 +37,21 @@ def add():
 
     with open(f'output/{filename}.csv', 'w') as parsed_file:
         write_config = csv.writer(parsed_file)
-        parsed_list = []
         for id in ids_list:
-            if is_plat_product(apikey, id):
+            product = get_plat_product(apikey, id, plat_authorization)
+            if 'status' in product and 'AVAILABLE' in product['status']:
                 write_config.writerow([id])
                 parsed_list.append(id)
+            else:
+                sanitized += 1
 
-    return render_template('list.html', apikey=apikey, list=parsed_list)
+    return render_template(
+        'list.html',
+        apikey=apikey,
+        list=parsed_list,
+        list_len=len(parsed_list),
+        sanitazed=sanitized
+    )
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import base64
+
+
+def make_auth(user, password):
+    auth_str = f'{user}:{password}'
+    auth_bytes = auth_str.encode('ascii')
+    base64_bytes = base64.b64encode(auth_bytes)
+    plat_authorization = base64_bytes.decode('ascii')
+    return f'Basic {plat_authorization}'
+
+
+def get_plat_product(apikey, id, auth):
+    try:
+        ENDPOINT = f'https://platform.chaordicsystems.com/raas/v2' \
+            f'/clients/{apikey}/products/{id}'
+        response = requests.get(
+            ENDPOINT,
+            headers={'Authorization': auth}
+        )
+        return json.loads(response.text)
+
+    except Exception as e:
+        print(f'Error on get_plat_product: {e}')
+        return {}
